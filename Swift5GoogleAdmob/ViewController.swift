1a639fa20af41416a975191e1d00ba5310cedcaa
ItakuraKayori <mamedaifuku2525@gmail.com>
2020-06-05 15:39:12 +0900

    Initial Commit

---
+++ Swift5GoogleAdmob/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5GoogleAdmob
+//
+//  Created by 板倉果保 on 2020/06/05.
+//  Copyright © 2020 Itakura Kayori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
